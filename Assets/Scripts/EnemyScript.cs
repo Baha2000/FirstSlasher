7a2fd649434a283925e170c0c0d081eb36c31cf2
Baha2000_Home <dragon.baha@yandex.ru>
2022-04-01 02:32:12 +0500

    Add few features

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public int EnemyHP = 100;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,23 +1,60 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
     private Rigidbody playerRigidBody;
+    private Vector3 currentPosition;
+    private bool isSpaceAttackStarted;
     private float speed;
+    private float shiftSpeed;
     public Camera camera;
     void Start()
     {
         playerRigidBody = gameObject.GetComponent<Rigidbody>();
-        speed = 2f;
+        speed = 20f;
+        shiftSpeed = 15f;
     }
 
     private void FixedUpdate()
     {
         RotationLogic();
         MovementLogic();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            isSpaceAttackStarted = true;
+            Vector3 speedUp = new Vector3(0f, 0f, 50f);
+            Vector3 startPoint = transform.position;
+            Vector3 endPoint = new Vector3(transform.position.x, transform.position.y, transform.position.z + 15f);
+            currentPosition = transform.position;
+            //playerRigidBody.MovePosition(endPoint);
+            //playerRigidBody.AddRelativeForce(Vector3.forward * 1000f, ForceMode.VelocityChange);
+            
+            //playerRigidBody.AddRelativeForce(Vector3.back * 1000f, ForceMode.VelocityChange);
+            //playerRigidBody.AddForce(Vector3.forward * 50f, ForceMode.Force);
+            //playerRigidBody.GetPointVelocity(endPoint);
+            //playerRigidBody.AddForceAtPosition(speedUp, endPoint);
+            //playerRigidBody.AddForceAtPosition(speedUp, startPoint);
+
+            //playerRigidBody.AddRelativeForce(endPoint, ForceMode.VelocityChange);
+            //playerRigidBody.AddRelativeForce(startPoint, ForceMode.VelocityChange);
+
+            //playerRigidBody.MovePosition(endPoint);
+            //playerRigidBody.MovePosition(startPoint);
+        }
+
+        if (isSpaceAttackStarted)
+        {
+            SpaceAttack();
+        }
     }
 
     private void RotationLogic()
@@ -71,8 +108,31 @@ public class PlayerMove : MonoBehaviour
          float moveVertical = Input.GetAxis("Vertical");
         
          Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-         playerRigidBody.AddRelativeForce(movement * speed, ForceMode.Impulse);
+         //playerRigidBody.AddRelativeForce(movement * speed, ForceMode.Impulse);
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            playerRigidBody.velocity = movement * (speed + shiftSpeed);
+        }
+        else
+        {
+            playerRigidBody.velocity = movement * speed;
+        }
+    }
 
-        //playerRigidBody.velocity += movement * speed;
+    private void SpaceAttack()
+    {
+        Vector3 endPoint = new Vector3(transform.position.x, transform.position.y, transform.position.z + 10000f);
+        if (transform.position == currentPosition)
+        {
+            playerRigidBody.AddRelativeForce(transform.forward * 100f, ForceMode.VelocityChange);
+            //playerRigidBody.AddRelativeForce(Vector3.forward * 500f, ForceMode.Impulse);
+        }
+
+        if (transform.position == endPoint)
+        {
+            //playerRigidBody.AddRelativeForce(Vector3.back * 500f, ForceMode.Impulse);
+            isSpaceAttackStarted = false;
+        }
     }
 }
